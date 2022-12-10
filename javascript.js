8ba382b48a3df4753f87949354cf9fe38cdedb98
Joe <flightbuzz1234@gmail.com>
2022-12-10 01:32:38 +0000

    added delete book

--- javascript.js
+++ javascript.js
@@ -28,7 +28,10 @@ userBook.addEventListener("click", (e) => {
   pages = document.querySelector("#bookPages").value;
   read = document.querySelector("#hasRead").checked;
   addBookToLibrary(createBook());
+  creatCard();
 });
+
+//Event listener for delete buttons
 
 //Our constructor
 const Book = {
@@ -60,27 +63,34 @@ function createBook() {
   }
 }
 
+let deleteButton = document.querySelectorAll(".delete");
+
 function creatCard() {
   //Forgive, I don't know a better way to do this, yet.
   //Hopefully react will save me in the future
+  let bookNumber = myLibrary.length;
+
   const books = document.querySelector(".books");
   const card = document.createElement("div");
   card.classList.add("card");
+  card.setAttribute("id", `a${bookNumber}`);
   card.innerHTML = `
-  <card class="info">
-      <p class="title">The Dream of all things Possible</p>
-      <p class="author">by Tolkien</p>
-      <p class="pages">234 pages</p>
+  <card class="info" id="a${bookNumber}">
+      <p class="title" id="a${bookNumber}">${title}</p>
+      <p class="author" id="a${bookNumber}">by ${author}</p>
+      <p class="pages" id="a${bookNumber}">${pages}</p>
       <div>
-          <p class="status">Status:</p>
-          <span>Read</span>
+          <p class="status" id="a${bookNumber}">Status:</p>
+          <span>${read}</span>
       </div>
   </card>
-  <div class="cardButton"><button class="delete">Delete</button></div>
+  <div class="cardButton" id="a${bookNumber}">
+  <button class="delete"onclick="deleteCard(this.id)" id="a${bookNumber}">Delete</button></div>
 </div>`;
-
-  card.classList.add("card");
+  //Card gets added
   books.appendChild(card);
 }
 
-log(myLibrary);
+function deleteCard(id) {
+  document.getElementById(`${id}`).remove();
+}

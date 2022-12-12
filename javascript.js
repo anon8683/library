5f47fe1c4997264c12f436d2031395877d1b5146
Joe <flightbuzz1234@gmail.com>
2022-12-12 12:14:23 +0000

    detailed js comments for reference and code clean, removal of log etc

--- javascript.js
+++ javascript.js
@@ -1,8 +1,16 @@
-let log = console.log;
-
 const form = document.querySelector("form");
 const userBook = document.querySelector("#submit");
+let deleteButton = document.querySelectorAll(".delete");
+// Our array of books
+let myLibrary = [];
 
+//Variables
+let title = "";
+let author = "";
+let pages = undefined;
+let read = undefined;
+
+//Displays form when button is clicked
 function openForm() {
   document.querySelector("form").style.display = "flex";
   document.getElementById("form").style.display = "block";
@@ -14,16 +22,8 @@ function closeform() {
   document.getElementById("form").style.display = "none";
 }
 
-// Our array of books
-let myLibrary = [];
-
-let title = "";
-let author = "";
-let pages = undefined;
-let read = undefined;
-
 //Results of our users form inputs, puts results into our variables
-//Then creates our book, and adds it to our array
+//Then creates our book, and adds it to our array, creates card from given inputs
 userBook.addEventListener("click", (e) => {
   title = document.querySelector("#bookTitle").value;
   author = document.querySelector("#bookAuthor").value;
@@ -31,13 +31,13 @@ userBook.addEventListener("click", (e) => {
   read = document.querySelector("#hasRead").checked;
   addBookToLibrary(createBook());
   createCard();
+
+  //Reset input values after card creation
   document.querySelector("#bookTitle").value = "";
   document.querySelector("#bookAuthor").value = "";
   document.querySelector("#bookPages").value = "";
   document.querySelector("#hasRead").checked = undefined;
 });
-
-//Event listener for delete buttons
 
 //Our constructor
 const Book = {
@@ -54,8 +54,6 @@ const Book = {
 function addBookToLibrary(book) {
   myLibrary.push(book);
   adjustStorage();
-
-  log(myLibrary);
 }
 
 //This function creates a book object
@@ -70,43 +68,54 @@ function createBook() {
   }
 }
 
-let deleteButton = document.querySelectorAll(".delete");
-
 function totalbooks() {
+  //Updates our book stats text content
   const totalBooks = document.getElementById("totalBooksResult");
   const booksRead = document.getElementById("booksReadResult");
 
+  // Our array without null spaces, so we can get the real length
   let realTotal = myLibrary.filter((n) => n);
+  // total amount of books is equal to the length of library without null
   totalBooks.textContent = `${realTotal.length}`;
 
+  // iterates through null free array, if the object.read is true, add to amount read
   let read = 0;
-
   for (let i = 0; i < realTotal.length; i++) {
     if (realTotal[i].read === true) {
       read++;
     }
   }
+  //Sets text content to amount of books read, after for loop has finished
   booksRead.textContent = `${read}`;
 
+  // books unread is equal to total books - readBooks
   const booksUnread = document.getElementById("booksUnreadResult");
   let unreadBooks = realTotal.length - read;
   booksUnread.textContent = `${unreadBooks}`;
 }
 
 function createCard() {
-  //Forgive, I don't know a better way to do this, yet.
-  //Hopefully react will save me in the future
+  //Calls totalBooks to update the stats on card creation
   totalbooks();
   const books = document.querySelector(".books");
   const card = document.createElement("div");
 
+  // Lets us get unique ID's by using the length of library
   let bookNumber = myLibrary.length;
+
+  // Card checkbox is default not checked, if user has read the book,
+  // set to Checked
   let checkBoxStatus = "";
   if (read === true) {
     checkBoxStatus = "checked=true";
   }
 
+  //Our card HTML that will be created
+  //Book number will give unique ID's, "a" is before bookNumber because ID's can't start with
+  // a number
   card.classList.add("card");
+
+  //add class based on read status, changes card background
   card.classList.add(`${read}`);
   card.setAttribute("id", `a${bookNumber}`);
   card.innerHTML = `
@@ -129,19 +138,31 @@ function createCard() {
 
 // Clicking a chexkbox will update it's attribute to check or not checked depending on previous state
 function changeStatus(id) {
+  // ID of element clicked is parsed in, and then stripped at -1,
+  // because our last character will always be the unique ID number
+  // then minus 1 to get our index of the book object being updated
+
   let box = document.getElementById(`${id}`);
   let bookIndex = +id.slice(-1) - 1;
   let card = document.getElementById(`a${bookIndex + 1}`);
+
+  // if the checkbox clicked does not have "checked" attribute then read = false,
+  // give it "checked" attribute and use the index to change "read" on the object to true,
+  // and give the card a class of true
 
   if (box.getAttribute("checked") === null) {
     box.setAttribute("checked", "true");
     myLibrary[bookIndex].read = true;
     card.classList.add("true");
+
+    // adjust our storage with the updated object and our book stats since
+    // the amount of read/not read books has changed
     adjustStorage();
     totalbooks();
 
     return;
   }
+  //otherwise remove checked attribute and class
   box.removeAttribute("checked");
   myLibrary[bookIndex].read = false;
   card.classList.remove("true");
@@ -149,9 +170,13 @@ function changeStatus(id) {
   totalbooks();
 }
 
+//
 function deleteCard(id) {
+  // ID is parsed in when delete is clicked, index of object to delete id-1
+  // remove all elements from the DOM with the given ID
+  // delete the object from the library Array, leaves a null space because of delete
+
   let index = +id.slice(1) - 1;
-  log(index);
   document.getElementById(`${id}`).remove();
   delete myLibrary[index];
   totalbooks();
@@ -165,6 +190,7 @@ function callRandom() {
   }
 }
 
+// When random button is clicked, add a random set of books from this function
 function addRandomBooks() {
   let randomRead = [true, false, true, false];
 
@@ -200,10 +226,12 @@ function addRandomBooks() {
   createCard();
 }
 
+//turns our library into a string and puts it into storage
 function adjustStorage() {
   localStorage.setItem("books", JSON.stringify(myLibrary));
 }
 
+//returns our library string and parses it into objects while removing all null spaces
 function getStorage() {
   let saved_books = JSON.parse(localStorage.getItem("books")).filter((n) => n);
   return saved_books;
@@ -214,10 +242,7 @@ function getStorage() {
 // For each item in new array, create a book from it
 window.addEventListener("load", (e) => {
   if (localStorage.getItem("books") != null) {
-    let saved_books = JSON.parse(localStorage.getItem("books")).filter(
-      (n) => n
-    );
-    log(saved_books);
+    let saved_books = getStorage();
     for (let index = 0; index < saved_books.length; index++) {
       title = saved_books[index].title;
       author = saved_books[index].author;
